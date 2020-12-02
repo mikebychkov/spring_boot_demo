f9147940767a97f018d306f26aabe3b65ce736e8
Admin <mihailbichkov@yandex.ru>
2020-12-02 18:20:07 +0500

    beginning with thymeleaf

---
+++ src/main/java/com/example/demo/control/HelloController.java
@@ -0,0 +1,36 @@
+package com.example.demo.control;
+
+import com.example.demo.service.EmployeeSvc;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
+
+@Controller
+public class HelloController {
+
+    private final EmployeeSvc employeeSvc;
+
+    @Autowired
+    public HelloController(EmployeeSvc employeeSvc) {
+        this.employeeSvc = employeeSvc;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+
+        model.addAttribute("emps", employeeSvc.getAll());
+
+        return "index";
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+
+        model.addAttribute("theDate", new Date());
+
+        return "helloworld";
+    }
+}
